ba91c5ffe6c427b2b4836efadcd94e113e44ec42
André <tdc@gmx.ch>
2020-03-08 19:29:46 +0100

    add rest example

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
+# importing the requests library 
+import requests 
 
 print('hello welt')
+
+URL = "https://api.predic8.de/shop/products/"
+
+response = requests.get(url = URL)
+data = response.json() 
+
+print(data)
